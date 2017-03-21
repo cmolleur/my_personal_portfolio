1f4bdda1c0a6f258108a824d0d33a9d3d09a2a0d
Colette Molleur <colette.molleur@gmail.com>
2017-03-21 12:18:28 -0400

    adds new portfolio files

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
   $(".design").hide();
+  $(".ux").hide();
   $("#web").on("click", function(){
       $(".web").fadeIn();
       $(".design").hide();
+      $(".ux").hide();
   });
   $("#design").on("click", function(){
       $(".design").fadeIn();
       $(".web").hide();
+      $(".ux").hide();
+  });
+  $("#ux").on("click", function(){
+      $(".ux").fadeIn();
+      $(".web").hide();
+      $(".design").hide();
   });
 });
